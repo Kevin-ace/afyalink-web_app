5c55d72c9a5be9b8968ceee14b2475ab0bcff723
Kevin <kevinkipruto83@gmail.com>
2025-03-28 08:19:52 +0300

    [UPDATE]app.py(run with host=0.0.0.0 so the  app listens on all interfaces)

--- web_app/app.py
+++ web_app/app.py
@@ -184,6 +184,10 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+import os
+
 if __name__ == '__main__':
     load_facilities_from_csv()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT, default to 5000 if not set
+    app.run(host="0.0.0.0", port=port, debug=True)  # Consider turning off debug in production!
+
